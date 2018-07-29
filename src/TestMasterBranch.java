f03fc1e879eb0960ac376cf52ad7d7a89f2582a4
SumiMuhammad <41795790+SumiMuhammad@users.noreply.github.com>
2018-07-29 18:51:34 +0700

    Master Branch
    Filed edited

--- src/TestMasterBranch.java
+++ src/TestMasterBranch.java
@@ -1,5 +1,9 @@
 public class TestMasterBranch {
 
     String name="test";
-
+    String country = "Pakistan";
+    
+    public void Method1(){
+        System.out.println("This is methos 1");
+    }
 }
